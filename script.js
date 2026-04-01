df2690482a6b22b673532eda9baa9cf8e310259d
elamranimohameddev-collab <elamranimohamed.dev@gmail.com>
2026-04-01 02:59:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 document.getElementById('start-btn').addEventListener('click', function() {
     let resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = "جاري القياس... تسنى شوية ⏳";
-    resultDiv.style.color = "#ffc107"; // لون صفر باش يبان راه خدام
+    // غنبدلو الميساج باش نعلمو المستخدِم بلي غيتعطل شوية
+    resultDiv.innerHTML = "جاري القياس... تسنى شوية ⏳<br><span style='font-size:14px; color:#666;'>التيست كياخد شوية دالوقت باش يعطي نتيجة دقيقة</span>";
+    resultDiv.style.color = "#ffc107";
     
-    // تصويرة باش نتيستيو بيها السرعة
-    let imageAddr = "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg" + "?n=" + Math.random(); 
-    let downloadSize = 1350000; // الحجم ديالها التقريبي بالـ Bytes
+    // استعملنا تصويرة كبيرة ديال 10 ميڭا باش التيست يطول والسرعة تستقر
+    let imageAddr = "https://upload.wikimedia.org/wikipedia/commons/f/ff/Pizigani_1367_Chart_10MB.jpg" + "?n=" + Math.random(); 
+    let downloadSize = 10485760; // 10 MB بالبايت (Bytes)
     let startTime, endTime;
     let download = new Image();
 
@@ -23,13 +24,15 @@ document.getElementById('start-btn').addEventListener('click', function() {
     download.src = imageAddr;
 
     function showResults() {
-        let duration = (endTime - startTime) / 1000; // شحال ديال الثواني
-        let bitsLoaded = downloadSize * 8;
+        let duration = (endTime - startTime) / 1000; // الوقت بالثواني
+        let bitsLoaded = downloadSize * 8; // نحولو لـ Bits
         let speedBps = (bitsLoaded / duration).toFixed(2);
         let speedKbps = (speedBps / 1024).toFixed(2);
         let speedMbps = (speedKbps / 1024).toFixed(2);
         
-        resultDiv.innerHTML = "السرعة ديالك هي: <br><span style='font-size:35px; color:#28a745;'>" + speedMbps + " Mbps</span>";
+        // غنقصو 10% ديال Overhead باش نقربو للنتيجة الحقيقية ديال سيرفورات القياس
+        let finalSpeed = (speedMbps * 0.90).toFixed(2);
+        
+        resultDiv.innerHTML = "السرعة ديالك هي: <br><span style='font-size:35px; color:#28a745;'>" + finalSpeed + " Mbps</span>";
     }
 });
- 
